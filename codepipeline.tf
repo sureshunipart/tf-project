ae180c5e0f119e17fe68fb253107d41904f66c25
ESPIRE\suresh.sharma <sureshsharma19822525@gmail.com>
2023-02-08 21:09:45 +0530

    daa

--- codepipeline.tf
+++ codepipeline.tf
@@ -69,7 +69,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             output_artifacts = ["tf-code"]
             configuration = {
                 FullRepositoryId = "sureshunipart/tf-project"
-                BranchName   = "master"
+                BranchName   = "main"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
             }
